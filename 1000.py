a06b60c823086e719982dcc0ab4b9135a38e30ec
minlee <kkang47140@gmail.com>
2021-08-23 23:12:30 +0900

    210823

--- 1000.py
+++
@@ -1,3 +0,0 @@
-from random import *
-date =  randint(4, 28)
-print("오프라인"+ str(date)+"일")
--- Reverse_String_Pythonic_Way.py
+++
@@ -1,6 +0,0 @@
-def reverseString(s:list[str]) -> None:
-    s.reverse()
-
-str = ["h","t","y"]
-ans = reverseString(str)
-print(ans)
